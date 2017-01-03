8d7668afcd44a3bb06029218044057bf543dfe26
santiago92 <santi.alasia92@gmail.com>
2017-01-02 23:41:23 -0300

    initial fix

--- init_environment.py
+++ init_environment.py
@@ -4,7 +4,7 @@ import os
 TANNER_LOCATION = "/opt/tanner/bin"
 TANNER_PORT = 8090
 def check_port():
-   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex(('127.0.0.1',TANNER_PORT))
     if result == 0:
         return True
